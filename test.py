e1fcf8eac8380b52a919ad5e1e33c1ae0909811d
Jump T. Wong <awong1900@163.com>
2020-06-05 00:43:55 +0800

    no message

---
+++ test.py
@@ -0,0 +1,23 @@
+"""Test."""
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitl_wait(3)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the browser!')
+        # assert 'To-Do' in browser.title, 'browser title was ' + browser.title
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
